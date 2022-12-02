898ff61786816c8e13f53eb9445cc4079dbe7d5e
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2022-12-02 16:21:53 +0100

    feat: day 2

---
+++ day2/day2.py
@@ -0,0 +1,22 @@
+def calculate_score(elf, me):
+    score = 3 * (me == elf) + 6 * (me == elf + 1 or me == elf - 2)
+    return me + score
+
+
+def get_my_shape(elf, me):
+    my_shape = (points[elf] + (me == "Z") - (me == "X")) % 3
+    return 3 if my_shape == 0 else my_shape
+
+
+points = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+total_score_pt1 = 0
+total_score_pt2 = 0
+
+for line in open("day2/input.txt").readlines():
+    elf, me = line.split()
+    total_score_pt1 += calculate_score(points[elf], points[me])
+    my_shape = get_my_shape(elf, me)
+    total_score_pt2 += calculate_score(points[elf], my_shape)
+
+print(total_score_pt1)
+print(total_score_pt2)
