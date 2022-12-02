e5a43ccfdb72e5abc99b860c65337456f14e8c1d
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2022-12-02 08:06:41 +0100

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,12 @@
+calories = []
+elfs_calories = 0
+
+for line in open("day1/input.txt", "r").readlines():
+    if line != '\n':
+        elfs_calories += int(line)
+    else:
+        calories.append(elfs_calories)
+        elfs_calories = 0
+
+print(max(calories))
+print(sum(sorted(calories)[-3:]))
