5c469d84a846140b27d55016a7c63076bc3a2939
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2022-12-04 10:36:42 +0000

    feat: day4

---
+++ day4/day4.py
@@ -0,0 +1,15 @@
+contains = []
+overlaps = []
+
+for line in open("day4/input.txt").readlines():
+    pair = line.split(",")
+    e1, e2 = [tuple(map(int, e)) for e in [elf.split("-") for elf in pair]]
+    contains.append(
+        (e1[0] >= e2[0] and e1[1] <= e2[1]) or (e1[0] <= e2[0] and e1[1] >= e2[1])
+    )
+    overlaps.append(
+        (e1[0] <= e2[1] and e1[1] >= e2[0]) or (e1[1] >= e2[0] and e1[0] <= e2[1])
+    )
+
+print(contains.count(True))
+print(overlaps.count(True))
