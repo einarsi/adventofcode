22522ee174a0357a62e367e1fc4fea98ab6bc1d7
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2022-12-06 15:24:47 +0000

    refactor(day6): use deque, not  range+slices

--- day6/day6.py
+++ day6/day6.py
@@ -1,7 +1,22 @@
+from collections import deque
+
 data = open("day6/input.txt").read().strip()
 
+# Deque makes the code more readable, less error prone to off-by-one
+# errors and should be faster. Also more suited for an actual stream,
+# since characters can be added as they arrive.
+
 for ln in [4, 14]:
-    for i in range(ln, len(data) + 1):
-        if len(set(data[i - ln : i])) == ln:
+    d = deque(maxlen=ln)
+    for i, c in enumerate(data):
+        d.append(c)
+        if len(set(d)) == ln:
             break
-    print(i)
+    print(i + 1)
+
+# Original solution
+# for ln in [4, 14]:
+#     for i in range(ln, len(data) + 1):
+#         if len(set(data[i - ln : i])) == ln:
+#             break
+#     print(i)
