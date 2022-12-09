7f79dbc2b1b954a4f5aa16955a196333ed5cd0bf
Einar S. Idsø <2356425+einarsi@users.noreply.github.com>
2022-12-09 18:14:18 +0000

    refactor(day9): simplify move calculation

--- day09_rope_bridge/day9.py
+++ day09_rope_bridge/day9.py
@@ -1,27 +1,23 @@
-def sign(x):
-    return (1, -1)[x < 0]
-
-
 lines = [line.strip() for line in open("input.txt")]
 
 DIR = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
-pos = [(0, 0) for _ in range(10)]  # head at 0
-all_positions = [set([(0, 0)]) for _ in range(10)]
+knots = [(0, 0) for _ in range(10)]  # head at 0
+positions = [set([(0, 0)]) for _ in range(10)]
 
 for line in lines:
     d, n = line.split()
     for _ in range(int(n)):
         move_x, move_y = DIR[d]
-        pos[0] = (pos[0][0] + move_x, pos[0][1] + move_y)
-        for k in range(1, len(pos)):
-            dx, dy = (pos[k - 1][0] - pos[k][0], pos[k - 1][1] - pos[k][1])
+        knots[0] = (knots[0][0] + move_x, knots[0][1] + move_y)
+        for k in range(1, len(knots)):
+            dx, dy = (knots[k - 1][0] - knots[k][0], knots[k - 1][1] - knots[k][1])
             if max([abs(dx), abs(dy)]) <= 1:
                 continue
-            move_x = (abs(dx) > 1 or (abs(dy) > 1 and abs(dx) != 0)) * sign(dx)
-            move_y = (abs(dy) > 1 or (abs(dx) > 1 and abs(dy) != 0)) * sign(dy)
-            pos[k] = (pos[k][0] + move_x, pos[k][1] + move_y)
-            all_positions[k].add(pos[k])
+            move_x = (dx > 0) - (dx < 0)
+            move_y = (dy > 0) - (dy < 0)
+            knots[k] = (knots[k][0] + move_x, knots[k][1] + move_y)
+            positions[k].add(knots[k])
 
-print(len(all_positions[1]))  # 6269
-print(len(all_positions[9]))  # 2557
+print(len(positions[1]))  # 6269
+print(len(positions[9]))  # 2557
